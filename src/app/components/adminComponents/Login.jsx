a6526e9a7161f4ec7ad33a93499a44ac94a5a25b
Pang Hangzhi <hangzhipang@gmail.com>
2019-03-24 16:00:23 +0800

    add axios and login

--- src/app/components/adminComponents/Login.jsx
+++ src/app/components/adminComponents/Login.jsx
@@ -1,9 +1,10 @@
 import * as React from 'react';
 import { Jumbotron } from 'reactstrap';
 import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import Axios from 'axios';
 
 class Login extends React.Component {
-    email = '';
+    username = '';
     password = '';
 
     constructor(props) {
@@ -14,11 +15,19 @@ class Login extends React.Component {
     }
 
     LoginSubmit = () => {
-      sessionStorage.setItem('loggedIn', JSON.stringify({
-        email: this.email,
-        role: this.email === 'supplier' ? 'SUPPLIER' : 'SHIPPING'
-      }));
-      this.props.history.push('/auth');
+      Axios.post('http://172.23.202.175:8010/login', 
+        {
+          username: this.username,
+          password: this.password
+      }).then(function(res) {
+        console.log(res);
+        sessionStorage.setItem('loggedIn', JSON.stringify({
+          username: res.username,
+          role: res.role
+        }));
+      })
+     
+  
     }
 
     render() {
@@ -27,11 +36,11 @@ class Login extends React.Component {
             <Jumbotron style={{width:'100%',borderRadius:0}}>
               <Form>
                 <FormGroup>
-                  <Label for="exampleEmail">Email</Label>
-                  <Input type="email" name="email" id="email" placeholder="example@email.com" onChange={(e) => this.email = e.target.value} />
+                  <Label for="exampleUsername">Username</Label>
+                  <Input type="username" name="username" id="username" placeholder="user name" onChange={(e) => this.username = e.target.value} />
                 </FormGroup>
                 <FormGroup>
-                  <Label for="exampleEmail">Password</Label>
+                  <Label for="exampleUsername">Password</Label>
                   <Input type="password" name="password" id="password" onChange={(e) => this.password = e.target.value} />
                 </FormGroup>
                 <Button onClick={this.LoginSubmit}>Submit</Button>
