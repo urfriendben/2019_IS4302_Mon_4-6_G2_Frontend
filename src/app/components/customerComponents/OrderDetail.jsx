86afe25d9985fd9a9f431d9b72b8f478cd81694a
Pang Hangzhi <hangzhipang@gmail.com>
2019-03-26 19:05:46 +0800

    fix view order detail issue

--- src/app/components/customerComponents/OrderDetail.jsx
+++ src/app/components/customerComponents/OrderDetail.jsx
@@ -1,45 +1,51 @@
 import * as React from 'react';
+import Axios from 'axios';
 
 class OrderDetail extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             error: null,
-            isLoaded: false,
+            isLoadedGoods: false,
             productId: this.props.match.params.orderId,
             orderInfo: null,
-            products: null,
+            goodsId: null,
+            goodsName: null,
+            price: null,
+            type: null,
             quantity: null, 
         };
     }
 
     componentDidMount() {
-        fetch("http://localhost:8010/order/" + this.state.productId)
-            .then(res => res.json())
-            .then(
-                (result) => {
-                    console.log(result)
-                    this.setState({
-                        isLoaded: true,
-                        orderInfo: result.orderInfo,
-                        products: result.orderInfo.goods,
-                        quantity: result.orderInfo.quantity
-                    });
-                },
-                (error) => {
-                    this.setState({
-                        isLoaded: true,
-                        error
-                    });
-                }
-            )
+        var self = this;
+        Axios.post('http://localhost:8010/order/' + this.state.productId,  {
+            })
+            .then(function (result) {
+                
+                console.log(result);
+              self.setState({
+                isLoadedGoods: true,
+                // products: result.orderInfo.goods,
+                orderInfo: result.data.orderInfo,
+                goodsId: result.data.goodsId,
+                goodsName: result.data.goodsName,
+                price: result.data.price,
+                type: result.data.type,
+                quantity: result.data.quantity
+            });
+            })
+            .catch(function (error) {
+              console.log(error);
+              alert(error);
+            });
     }
 
     render() {
-        const { error, isLoaded, orderInfo, products, quantity} = this.state;
+        const { error, isLoadedGoods, orderInfo, goodsId, goodsName, price, type, quantity} = this.state;
         if (error) {
             return <div className="container"><div>Error: {error.message}</div></div>;
-        } else if (!isLoaded) {
+        } else if (!isLoadedGoods ) {
             return <div className="container"><div>Loading...</div></div>;
         } else {
             return (
@@ -51,24 +57,27 @@ class OrderDetail extends React.Component {
                     <thead>
                     <tr>
                         <th>Product ID</th>
-                        {/* <th>Product Name</th>
-                        <th>Type</th> */}
+                        <th>Product Name</th>
+                        <th>Type</th>
                         <th>Quantity</th>
-                        {/* <th>Price</th> */}
+                        <th>Price</th>
                     </tr>
                     </thead>
                     <tbody>
-                    {products.map((p, index) => (
-                        <tr key={p.split("#")[1]}>
-                            <td><a href={"/customer/product/"+p.split("#")[1]}>{p.split("#")[1]}</a></td>
-                            {/* <td>{p.name}</td> */}
-                            {/* <td>{p.type}</td> */}
+                    {goodsId.map((p, index) => (
+                        <tr key={p}>
+                            <td><a href={"/customer/product/"+p}>{p}</a></td>
+                            <td>{goodsName[index]}</td>
+                            <td>{type[index]}</td>
                             <td>{quantity[index]}</td>
-                            {/* <td>{p.price * quantity[index]}</td> */}
+                            <td>{price[index] * quantity[index]}</td>
                         </tr>
                     ))}
                     </tbody>
                 </table>
+                <div className="alert alert-warning">
+                        <h3 className="total-price">Total Price: {orderInfo.totalPrice}</h3>
+                        </div>
             </div>
                 </div>)
         }
